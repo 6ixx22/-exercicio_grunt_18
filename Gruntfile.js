52be937d93728aa037a29378ae7c25a87810142c
Arthur Miguel <jemergemer446@gmail.com>
2024-09-18 15:04:12 -0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+  
+      less: {
+        development: {
+          options: {
+            compress: false, 
+          },
+          files: {
+            "css/styles.css": "less/main.less" 
+          }
+        },
+        production: {
+          options: {
+            compress: true, 
+          },
+          files: {
+            "css/styles.min.css": "less/main.less" 
+          }
+        }
+      },
+  
+      uglify: {
+        options: {
+          mangle: false, 
+          compress: true, 
+          sourceMap: true 
+        },
+        my_target: {
+          files: {
+            "js/scripts.min.js": ["js/scripts.js"] 
+          }
+        }
+      },
+  
+      watch: {
+        styles: {
+          files: ["less/**/*.less"],
+          tasks: ["less:development"]
+          options: {
+            spawn: false
+          }
+        },
+        scripts: {
+          files: ["js/**/*.js"], 
+          tasks: ["uglify"], 
+          options: {
+            spawn: false
+          }
+        }
+      }
+    });
+  
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask("default", ["less:development", "uglify"]);
+    grunt.registerTask("prod", ["less:production", "uglify"]);
+  };
+  
